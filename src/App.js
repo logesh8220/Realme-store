08d68a381768bd0a90b1b7562c4cfed42b74ae12
logesh8220 <logeshthirumurugan@gmail.com>
2023-05-16 16:58:21 +0530

    socket

--- src/App.js
+++ src/App.js
@@ -18,9 +18,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   // console.log(data);
-  const socket = io(env.api)
-  socket.on("connect", () => {
-  })
+  const socket = io.connect()
   socket.on("recive-notification", message => {
     toast.success(message, {
     });
